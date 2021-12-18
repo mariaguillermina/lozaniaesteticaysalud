5500dccfea121b294bff157c383dcef0e9c98f23
Guille <guillerminaup@gmail.com>
2021-12-18 13:14:27 -0500

    interaccion html

--- JavaScript/tienda.js
+++ JavaScript/tienda.js
@@ -10,17 +10,18 @@ class Carrito{
 
 // Declaración de Array//
 
-const arr1 = []
+const misProductos = [];
 
 //Objeto//
 
 class Producto{
-    constructor(id,nombre,precio,cantidad,stock = true){
+    constructor(id,nombre,precio,cantidad,img,stock = true){
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.cantidad = cantidad;
         this.stock = stock;
+        this.img = img;
 
     }
     getNombre(){
@@ -48,13 +49,13 @@ class Producto{
 
 //Lista de Productos//
 
-const producto1 = new Producto(1,'Kit Limpieza Facial',2500,1000,true);
+const producto1 = new Producto(1,'Kit Limpieza Facial',2500,1000,'./SASS/img/kit limpieza.jpg',true);
 
-const producto2 = new Producto(2,'Kit Vitamina C',2500,800,true);
+const producto2 = new Producto(2,'Kit Vitamina C',2500,800,'./SASS/img/kit radian c.jpg',true);
 
-const producto3 = new Producto(3,'Kit Anti Acné',2000,100,true);
+const producto3 = new Producto(3,'Kit Anti Acné',2000,100,'./SASS/img/acne kit.jpg',true);
 
-const producto4 = new Producto(4,'Protector Solar',1500,2000,true);
+const producto4 = new Producto(4,'Protector Solar',1500,2000,'./SASS/img/protector.jpg',true);
 
 
 //Carrito//
@@ -62,3 +63,5 @@ const producto4 = new Producto(4,'Protector Solar',1500,2000,true);
 const carrito = new Carrito([producto1,producto2,producto3,producto4])
 
 console.log( carrito.listarItems())
+
+let cards = document.getElementsByTagName('body');
