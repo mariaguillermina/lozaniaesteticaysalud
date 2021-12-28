288ab22efd64cac68ceed7e751a2903a49a452d4
Guille <guillerminaup@gmail.com>
2021-12-28 08:10:44 -0500

    interaccion HTML

--- JavaScript/tienda.js
+++ JavaScript/tienda.js
@@ -15,13 +15,14 @@ const misProductos = [];
 //Objeto//
 
 class Producto{
-    constructor(id,nombre,precio,cantidad,img,stock = true){
+    constructor(id,nombre,precio,cantidad,descripcion,img,stock = true){
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.cantidad = cantidad;
-        this.stock = stock;
+        this.descripcion = descripcion;
         this.img = img;
+        this.stock = stock;
 
     }
     getNombre(){
@@ -49,13 +50,13 @@ class Producto{
 
 //Lista de Productos//
 
-const producto1 = new Producto(1,'Kit Limpieza Facial',2500,1000,'./SASS/img/kit limpieza.jpg',true);
+const producto1 = new Producto(1,'Kit Limpieza Facial',2500,1000,'Incluye: Fórmula Limpieza, Plasma Infusion serum, Plasma Infusion crema, máscara de limpieza','./SASS/img/kit limpieza.jpg',true);
 
-const producto2 = new Producto(2,'Kit Vitamina C',2500,800,'./SASS/img/kit radian c.jpg',true);
+const producto2 = new Producto(2,'Kit Vitamina C',2500,800,'Incluye: Racian C Serum, Radian C crema facial.','./SASS/img/kit radian c.jpg',true);
 
-const producto3 = new Producto(3,'Kit Anti Acné',2000,100,'./SASS/img/acne kit.jpg',true);
+const producto3 = new Producto(3,'Kit Anti Acné',2000,100,'Incluye: Intensive Clearser, Tratamiento Acne detox, Tratamiento puntual acné.','./SASS/img/acne kit.jpg',true);
 
-const producto4 = new Producto(4,'Protector Solar',1500,2000,'./SASS/img/protector.jpg',true);
+const producto4 = new Producto(4,'Protector Solar',1500,2000,'Inluye: 1 protector solar','./SASS/img/protector.jpg',true);
 
 
 //Carrito//
@@ -64,4 +65,19 @@ const carrito = new Carrito([producto1,producto2,producto3,producto4])
 
 console.log( carrito.listarItems())
 
-let cards = document.getElementsByTagName('body');
+misProductos.push(producto1,producto2,producto3,producto4)
+
+let contenedorProductos = document.getElementById('container-productos')
+
+for (let producto of misProductos){
+    let div = document.createElement('div');
+    div.setAttribute('class','producto');
+
+    div.innerHTML = `
+    <img src="${producto.img}" alt="" width= "300">
+    <p type="texto" class="descripcion">${producto.descripcion}</p>
+    <button type="button" class="button-productos" id="productos${producto.id}">${producto.nombre}</button>`;
+
+    contenedorProductos.appendChild(div);
+
+}
